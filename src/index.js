39dba77b89069cc51fb639c8f52419b805fcb3de
hhh1361 <47751762+hhh1361@users.noreply.github.com>
2019-02-28 21:20:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    var quantity = {};
+    var sum = currency;
+    var rich = {}
+    rich.error = 'You are rich, my friend! We don\'t have so much coins for exchange'; 
+    
+    if (Math.floor(sum / 50) > 0) {
+        quantity.H = Math.floor(sum / 50)
+    sum = sum - quantity.H * 50; 
+    }; 
+    if (Math.floor(sum / 25) > 0) {
+        quantity.Q = Math.floor(sum / 25); 
+    sum = sum - quantity.Q * 25; 
+    }; 
+    if (Math.floor(sum / 10) > 0) {
+        quantity.D = Math.floor(sum / 10); 
+    sum = sum - quantity.D * 10; 
+    }; 
+    if (Math.floor(sum / 5) > 0) {
+        quantity.N = Math.floor(sum / 5); 
+    sum = sum - quantity.N * 5; 
+    }; 
+    if (Math.floor(sum / 1) > 0) {
+        quantity.P = Math.floor(sum / 1); 
+    sum = sum - quantity.P * 1; 
+    }
+    
+    return (currency > 10000)? rich : quantity;
+    }
